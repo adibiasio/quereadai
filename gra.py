5bf74ea1f904d32980a2f3bd7ac5ec4d46828766
Ethan Saddler <ethansaddler@Ethans-MacBook-Pro-2.local>
2023-10-14 00:58:04 -0400

    i hate authentication

--- gra.py
+++
@@ -1,8 +0,0 @@
-import gradio as gr
-
-# Gradio Function
-def greet(name):
-    return "Hello " + name + "!"
-
-# Create Gradio interface with share=True to get the URL
-gr.Interface(fn=greet, inputs="text", outputs="text", live=True).launch(share=True)
